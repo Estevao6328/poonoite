cc68905aff92eec8345cc82dcd7c829760bc67b4
maromo71 <professormoraes@gmail.com>
2020-02-18 19:17:33 -0300

    Aula do dia 18 de fevereiro

---
+++ prjApolice/src/com/company/Apolice.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Apolice {
+    public String nomeSegurado;
+    public int idade;
+    public float valorPremio;
+
+    public void imprimir(){
+        System.out.println("nomeSegurado = " + nomeSegurado);
+        System.out.println("idade = " + idade);
+        System.out.println("valorPremio = " + valorPremio);
+        System.out.println("-----------------------------");
+    }
+}
---
+++ prjApolice/src/com/company/GerenciarApolice.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class GerenciarApolice {
+
+    public static void main(String[] args) {
+	    Apolice apolice1 = new Apolice();
+	    apolice1.idade = 19;
+	    apolice1.nomeSegurado = "João Barbosa";
+	    apolice1.valorPremio = (float) 500000.0;
+	    apolice1.imprimir();
+    }
+}
